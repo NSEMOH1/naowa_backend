dd903b9ec4fa4998f7cfe43db873383b0fa78d59
UltimateProdigy <ayobamiakinola84@gmail.com>
2025-12-05 11:23:32 +0100

    fix: allowed origins

--- app.ts
+++ app.ts
@@ -36,6 +36,8 @@ const createApp = () => {
   app.use(
     cors({
       origin: config.allowedOrigins,
+      methods: ["GET", "POST", 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
       credentials: true,
     })
   );
